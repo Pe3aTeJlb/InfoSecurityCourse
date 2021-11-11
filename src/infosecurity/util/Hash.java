641061ef102716e577a2ab173d91fbb4ca84749c
Pe3aTeJlb <shepuhin@yandex.ru>
2021-11-11 23:47:10 +0300

    Custom hash

---
+++ src/infosecurity/util/Hash.java
@@ -0,0 +1,57 @@
+package infosecurity.util;
+
+import java.math.BigInteger;
+
+public class Hash {
+
+    private static BigInteger len;
+
+    public static String hash(String message){
+
+       // System.out.println("Hash input message : " + message);
+
+        StringBuilder msg = new StringBuilder(message);
+
+        return format(msg).substring(23,87);
+
+    }
+
+    private static String format(StringBuilder msg){
+
+      //  System.out.println("Format"+"\n");
+
+        msg = toBin(msg);
+        len = BigInteger.valueOf(msg.toString().length());
+
+        BigInteger val = new BigInteger(msg.toString(), 2);
+        val = val.pow(len.intValue());
+
+        for(int i = 0; i < 5; i++){
+
+            if(i % 2 == 0) {
+                val = val.xor(len.modPow(len, BigInteger.valueOf(3)));
+            }else{
+                val = val.pow(len.mod(val).intValue());
+            }
+
+        }
+
+        return val.toString(16);
+
+    }
+
+    private static StringBuilder toBin(StringBuilder msg){
+
+        StringBuilder buff = new StringBuilder();
+
+        for(int i = 0; i < msg.length(); i++){
+            String bin = String.format("%8s", Integer.toBinaryString(msg.charAt(i))).replace(" ","0");
+           // System.out.println(msg.charAt(i)+" "+bin);
+            buff.append(bin);
+        }
+
+        return buff;
+
+    }
+
+}
